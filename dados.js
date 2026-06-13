4cf7a3f6cd85cbf557a7f37ec5a7cbaadfcef587
rafamoreira08 <rafamoreira08@gmail.com>
2026-06-12 22:04:06 -0300

    Palpites completos 13/06 + 5 jogos novos de 14/06 (AUS, GER, NED, CIV, SWE)

--- dados.js
+++ dados.js
@@ -41,7 +41,7 @@ const JOGOS = [
     casa: "🇶🇦 Catar", fora: "Suíça 🇨🇭",
     abbrCasa: "QAT", abbrFora: "SUI",
     local: "📍 San Francisco",
-    palpites: null
+    palpites: { Rafa: [0,2], Xyko: [1,2], Teu: [0,3], Dani: [0,1], Soninha: [1,2], Lu: [0,2] }
   },
   {
     dia: "Sábado, 13 de junho", hora: "19h00", grupo: "C",
@@ -49,14 +49,49 @@ const JOGOS = [
     abbrCasa: "BRA", abbrFora: "MAR",
     local: "📍 Nova York / NJ",
     brasil: true,
-    palpites: null
+    palpites: { Rafa: [2,1], Xyko: [2,0], Teu: [2,1], Dani: [2,0], Soninha: [3,2], Lu: [3,0] }
   },
   {
     dia: "Sábado, 13 de junho", hora: "22h00", grupo: "C",
     casa: "🇭🇹 Haiti", fora: "Escócia 🏴󠁧󠁢󠁳󠁣󠁴󠁿",
     abbrCasa: "HAI", abbrFora: "SCO",
     local: "📍 Boston",
-    palpites: null
+    palpites: { Rafa: [1,2], Xyko: [0,3], Teu: [0,3], Dani: [1,1], Soninha: [0,2], Lu: [0,0] }
+  },
+  {
+    dia: "Domingo, 14 de junho", hora: "01h00", grupo: "D",
+    casa: "🇦🇺 Austrália", fora: "Turquia 🇹🇷",
+    abbrCasa: "AUS", abbrFora: "TUR",
+    local: "📍 Vancouver",
+    palpites: { Rafa: [0,0], Xyko: [1,1], Teu: [1,2], Dani: [1,0], Soninha: [1,1], Lu: [1,1] }
+  },
+  {
+    dia: "Domingo, 14 de junho", hora: "14h00", grupo: "E",
+    casa: "🇩🇪 Alemanha", fora: "Curaçao 🇨🇼",
+    abbrCasa: "GER", abbrFora: "CUW",
+    local: "📍 Houston",
+    palpites: { Rafa: [4,0], Xyko: [4,0], Teu: [5,0], Dani: [4,0], Soninha: [3,0], Lu: [5,0] }
+  },
+  {
+    dia: "Domingo, 14 de junho", hora: "17h00", grupo: "F",
+    casa: "🇳🇱 Holanda", fora: "Japão 🇯🇵",
+    abbrCasa: "NED", abbrFora: "JPN",
+    local: "📍 Dallas",
+    palpites: { Rafa: [1,2], Xyko: [3,1], Teu: [3,2], Dani: [2,0], Soninha: [2,1], Lu: [3,1] }
+  },
+  {
+    dia: "Domingo, 14 de junho", hora: "20h00", grupo: "E",
+    casa: "🇨🇮 C. do Marfim", fora: "Equador 🇪🇨",
+    abbrCasa: "CIV", abbrFora: "ECU",
+    local: "📍 Filadélfia",
+    palpites: { Rafa: [0,2], Xyko: [0,1], Teu: [1,2], Dani: [1,2], Soninha: [1,2], Lu: [0,1] }
+  },
+  {
+    dia: "Domingo, 14 de junho", hora: "23h00", grupo: "F",
+    casa: "🇸🇪 Suécia", fora: "Tunísia 🇹🇳",
+    abbrCasa: "SWE", abbrFora: "TUN",
+    local: "📍 Monterrey",
+    palpites: { Rafa: [1,0], Xyko: [3,0], Teu: [0,0], Dani: [2,0], Soninha: [2,0], Lu: [1,0] }
   }
 ];
 
